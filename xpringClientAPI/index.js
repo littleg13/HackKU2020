88f9536fa8037d268fb91390b62a23d8a8487eac
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2020-02-08 15:33:14 -0600

    Add newWallet endpoint

--- xpringClientAPI/index.js
+++ xpringClientAPI/index.js
@@ -5,6 +5,34 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = 5000;
 
+var mysql = require('mysql');
+// -------- DEV --------
+var con = mysql.createConnection({
+    host: "localhost",
+    database: "mooxter",
+    user: "root",
+    password: ""
+});
+// -------- PROD --------
+// var con = mysql.createConnection({
+//     host: "mysql",
+//     database: "",
+//     user: "root",
+//     password: "password"
+// });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to database");
+});
+
+// con.query("sql", function (err, result) {
+//     if (err) throw err;
+//     console.log("Result: " + result);
+// });
+
+const { XpringClient, Wallet } = require("xpring-js");
+
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,17 +70,101 @@ app.post('/api/v1/todos', (req, res) => {
     })
 });
 
-app.get('/api/v1/newWallet', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'todos retrieved successfully',
-    todos: db
-  })
+app.get('/api/v1/todos/:id', (req, res) => {
+    let response;
+    const id = parseInt(req.params.id, 10);
+    db.map((todo) => {
+        if (todo.id === id) {
+            response = res.status(200).send({
+                success: 'true',
+                message: 'todo retrieved successfully',
+                todo,
+            });
+        } 
+    });
+    if (response == null) {
+        return res.status(404).send({
+            success: 'false',
+            message: 'todo does not exist',
+        });
+    } else {
+        return response;
+    }
+});
+
+
+app.get('/api/v1/newWallet/:id', (req, res) => {
+    let response;
+    let query = `SELECT wallet_secret FROM users WHERE uid = '${req.params.id}'`;
+
+    try {
+        con.query(query, function (err, result) {
+            if (err) throw err;
+            if (result.length == 0) {
+                response = res.status(400).send({
+                    success: 'false',
+                    message: `Invalid ID given`
+                });
+                return;
+            } else if (result[0].wallet_secret != null) {
+                response = res.status(400).send({
+                    success: 'false',
+                    message: `ID already has a wallet`
+                });
+                return;
+            } else {
+                createWallet(res, req);
+            }
+        });
+    } catch (err) {
+        response = res.status(500).send({
+            success: 'false',
+            message: `Failed to check for pre-existing wallet, error: ${err}`
+        });
+    }
+});
+
+app.post('/api/v1/send', (req, res) => {
+  
+});
+
+app.get('/api/v1/balance:id', (req, res) => {
+  
+});
+
+app.get('/api/v1/transactionHistory:id', (req, res) => {
+  
+});
+
+app.post('/api/v1/deleteWallet:id', (req, res) => {
+  
 });
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 });
+
+function createWallet(res, req) {
+    const newWallet = Wallet.generateRandomWallet().wallet;
+
+    query = `UPDATE users SET wallet_address = '${newWallet.getAddress()}', wallet_secret = '${newWallet.getPrivateKey()}', \
+                wallet_public = '${newWallet.getPublicKey()}' WHERE uid = '${req.params.id}'`;
+    
+    try {
+        con.query(query, function (err, result) {
+            if (err) throw err;
+            res.status(200).send({
+                success: 'true',
+                message: 'Wallet created successfully',
+            });
+        });
+    } catch (err) {
+        return res.status(400).send({
+            success: 'false',
+            message: `Failed to create wallet, error: ${err}`
+        });
+    }
+}
 
 // const { XpringClient, Wallet } = require("xpring-js");
 
