c854942865bbfc847d6c508b82bbb872dc8fa45a
Eric <erjseals@gmail.com>
2020-02-08 17:14:25 -0600

    clearing up unused folders

--- WebServer/OAuth2/index.js
+++ WebServer/OAuth2/index.js
@@ -2,11 +2,13 @@ const http = require('http');
 const fs   = require('fs');
 const port = 53134;
 
+console.log("Starting Server on Port " + port);
+
 //Takes request and response
 //These are Node.js events (of http.Server)
-
 //Called at every http request to the server
 const requestListener = function (request, response) {
+
     let responseCode = 404;
     let content = '404 Error';
     
